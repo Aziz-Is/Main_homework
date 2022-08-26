d80bfa08acc228332f3193f32a89a75438dc40ed
Aziz_Is <Aziz10877@gmail.com>
2022-08-26 13:20:06 +0600

    registration added, fixtures added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
 # Create your views here.
 
@@ -17,8 +18,17 @@ def login_view(request):
             context['has error'] = True
     return render (request, 'login.html', context=context)
 
-
-
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+def register_user(request, *args, **kwargs):
+    if request.method == 'POST':
+        my_form = RegisterForm(request.POST)
+        if my_form.is_valid():
+            my_form.save()
+            return redirect('login')
+    else:
+        my_form = RegisterForm()
+    return render(request, 'register.html', {'form': my_form})
+
